83d6797490ce830d19b347e14e33a60725ced5de
Админ <macbahodir@gmail.com>
2024-06-05 12:22:30 +0500

    My homewok

---
+++ app.js
@@ -0,0 +1,95 @@
+// Let exercises
+
+//1
+
+// function darajaTopish(q, daraja) {
+//     let p = Math.pow(q, daraja);
+//     console.log(p);
+// }
+
+// darajaTopish(2 , 3);
+
+
+//2
+
+// function sonQoshish(l) {
+//     let q = [7]
+//     q.push(l)
+//     console.log(q); 
+// }
+
+// sonQoshish(2);
+
+
+//3
+
+//4
+
+// function koordinata(a, b) {
+//     if(a < b) {
+//         let masofa = b - a;
+//         console.log(masofa);
+//     }
+//     else if(b < a) {
+//         let masofa = a - b;
+//         console.log(masofa);
+//     }
+//     else {
+//         console.log("Bu nuqtalar orasidagi masofa 0ga teng!");
+//     }
+// }
+
+// koordinata(7, 3);
+
+//5
+
+
+//6
+
+// function tubSonYigindisi(n) {
+//     if(n)
+//     console.log(n);
+// }
+
+// tubSonYigindisi(3);
+
+//7
+
+//8
+
+// function berilganSon(n) {
+//     if(n % 11 == 0) {
+//         return console.log(1);
+//     }
+//     else {
+//         return console.log(0);
+//     }
+// }
+
+// berilganSon(23);
+
+//9
+
+// function almashtirish(son) {
+//     let ozgaruvchi = String(structuredClone(son));
+//     let k = ozgaruvchi[0];
+//     let n = ozgaruvchi[1];
+//     let natija = k.replace(k, n)
+//     console.log(natija);
+// }
+
+// almashtirish(17);
+
+//10
+
+// function tartiblash(son) {
+//     let tartiblanuvchiSon = son;
+// }
+
+// tartiblash(31);
+
+//11
+
+// function s(a, b, c) {
+    
+// }`
